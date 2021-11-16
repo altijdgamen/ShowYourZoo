b37b7333cded8f2599923566b1138aba774c59b1
Denzel01 <93382006+Denzel01@users.noreply.github.com>
2021-11-16 09:28:41 +0100

    2

--- javascript/dieren.js
+++
@@ -1,3 +0,0 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en'}, 'googleTranslate');
-  }
--- javascript/home.js
+++
@@ -1,8 +0,0 @@
-let audio = document.getElementById ('audio')
-window.onload = (event) => {
-    console.log('hallo')
-} 
-
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en'}, 'googleTranslate');
-}
